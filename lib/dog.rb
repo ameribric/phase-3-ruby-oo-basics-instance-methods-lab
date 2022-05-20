9ea9fa9582242d12c7a9527430475f78ab73766f
Amer Ibric <ameribric@gmail.com>
2022-05-20 12:34:56 -0400

    commit tree 7032c415acdeb987b744b3640b9922ec47fc8f79

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog
+    def bark 
+        puts "Woof!"
+    end
+
+    def eating_snack
+        puts "Yummy!"
+    end
+    
+    def sit 
+        puts "The Dog is sitting"
+    end
+
+    
+end
+
+fido = Dog.new
+fido.bark
+
+snoopy = Dog.new
+snoopy.eating_snack
+
